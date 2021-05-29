92b01cdba59c0e399569ed54ecdc4b114d8ed3e4
CrashBanditCoin14 <crashbanditcoin@gmail.com>
2021-05-29 17:13:06 +0200

    update compound all pools and added img

--- src/views/Farms/components/FarmCard/HarvestAction.tsx
+++ src/views/Farms/components/FarmCard/HarvestAction.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import BigNumber from 'bignumber.js'
-import { Button, Flex, Heading } from '@pancakeswap-libs/uikit'
+import {Button, Flex, Heading} from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
-import { useHarvest } from 'hooks/useHarvest'
-import { getBalanceNumber } from 'utils/formatBalance'
+import {useHarvest} from 'hooks/useHarvest'
+import {getBalanceNumber} from 'utils/formatBalance'
 import styled from 'styled-components'
 import useStake from '../../../../hooks/useStake'
 
 interface FarmCardActionsProps {
-  earnings?: BigNumber
-  pid?: number
+    earnings?: BigNumber
+    pid?: number
 }
 
 const BalanceAndCompound = styled.div`
@@ -19,49 +19,49 @@ const BalanceAndCompound = styled.div`
   flex-direction: column;
 `
 
-const HarvestAction: React.FC<FarmCardActionsProps> = ({ earnings, pid }) => {
-  const TranslateString = useI18n()
-  const [pendingTx, setPendingTx] = useState(false)
+const HarvestAction: React.FC<FarmCardActionsProps> = ({earnings, pid}) => {
+    const TranslateString = useI18n()
+    const [pendingTx, setPendingTx] = useState(false)
 
-  // compound to SUGAR pool
-  const { onReward } = useHarvest(3)
-  const { onStake } = useStake(3)
+    // compound to SUGAR pool
+    const {onReward} = useHarvest(pid)
+    const {onStake} = useStake(3)
 
-  const rawEarningsBalance = getBalanceNumber(earnings)
-  const displayBalance = rawEarningsBalance.toLocaleString()
+    const rawEarningsBalance = getBalanceNumber(earnings)
+    const displayBalance = rawEarningsBalance.toLocaleString()
 
-  return (
-    <Flex mb='8px' justifyContent='space-between' alignItems='center'>
-      <Heading color={rawEarningsBalance === 0 ? 'textDisabled' : 'text'}>{displayBalance}</Heading>
-      <BalanceAndCompound>
-        {pid === 0 || 1 || 2 || 3 || 4 || 5 || 6 ?
-          <Button
-            disabled={rawEarningsBalance === 0 || pendingTx}
-            size='sm'
-            variant='secondary'
-            marginBottom='15px'
-            onClick={async () => {
-              setPendingTx(true)
-              await onStake(rawEarningsBalance.toString())
-              setPendingTx(false)
-            }}
-          >
-            {TranslateString(999, 'Pack it')}
-          </Button>
-          : null}
-        <Button
-          disabled={rawEarningsBalance === 0 || pendingTx}
-          onClick={async () => {
-            setPendingTx(true)
-            await onReward()
-            setPendingTx(false)
-          }}
-        >
-          {TranslateString(999, 'Harvest')}
-        </Button>
-      </BalanceAndCompound>
-    </Flex>
-  )
+    return (
+        <Flex mb='8px' justifyContent='space-between' alignItems='center'>
+            <Heading color={rawEarningsBalance === 0 ? 'textDisabled' : 'text'}>{displayBalance}</Heading>
+            <BalanceAndCompound>
+                {pid === 0 || 1 || 2 || 3 || 4 || 5 || 6 ?
+                    <Button
+                        disabled={rawEarningsBalance === 0 || pendingTx}
+                        size='sm'
+                        variant='secondary'
+                        marginBottom='15px'
+                        onClick={async () => {
+                            setPendingTx(true)
+                            await onStake(rawEarningsBalance.toString())
+                            setPendingTx(false)
+                        }}
+                    >
+                        {TranslateString(999, 'Pack it')}
+                    </Button>
+                    : null}
+                <Button
+                    disabled={rawEarningsBalance === 0 || pendingTx}
+                    onClick={async () => {
+                        setPendingTx(true)
+                        await onReward()
+                        setPendingTx(false)
+                    }}
+                >
+                    {TranslateString(999, 'Harvest')}
+                </Button>
+            </BalanceAndCompound>
+        </Flex>
+    )
 }
 
 export default HarvestAction
