d1286fc523b37a6934d81c6109e039ac59bfdb07
CrashBanditCoin14 <crashbanditcoin@gmail.com>
2021-07-01 21:53:15 +0200

    add lottery feature

--- src/views/Lottery/components/TicketCard/PurchaseWarningModal.tsx
+++ src/views/Lottery/components/TicketCard/PurchaseWarningModal.tsx
@@ -10,11 +10,11 @@ const WarningModal: React.FC<{ onDismiss?: () => void }> = ({ onDismiss }) => {
   return (
     <Modal title={TranslateString(466, 'Warning')} onDismiss={onDismiss}>
       <TicketsList>
-        {TranslateString(468, 'Lottery ticket purchases are final.')}
+        Raffle ticket purchases are final.
         <br />
         Your SUGAR will not be returned to you after you spend it to buy tickets.
         <br />
-        {TranslateString(472, 'Tickets are only valid for one lottery draw, and will be burnedafter the draw.')}
+        Tickets are only valid for one raffle draw, and will be burnedafter the draw.
         <br />
         {TranslateString(
           474,
