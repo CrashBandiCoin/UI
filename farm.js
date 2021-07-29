1905da69305204fb99e5098448adf5d68100dbde
Developer <85023510+carlatic-dev@users.noreply.github.com>
2021-07-29 14:33:37 -0700

    Update cronjob

--- farm.js
+++ farm.js
@@ -1,10 +1,10 @@
 const SUGARU_STRATEGY_ABI = require('./abi/sugarStrategy.json')
 const CAKE_STRATEGY_ABI = require('./abi/cakeStrategy.json')
 
-const ownerAddress = "add owner address";
-const sugarStrategyAddress = "add sugar strategy address";
-const cakeStrategyAddress = "add cake strategy address";
-const privateKey = "add private key";
+const ownerAddress = "add your address";
+const sugarStrategyAddress = "0x297Fc82C7469B1019B8aEEBB74DF8dEdEA88911a";
+const cakeStrategyAddress = "0x4b1AE1871de7fA67AE61F7B6500211F38f2fEDAf";
+const privateKey = "add your private key";
 
 const Web3 = require('web3');
 
@@ -17,7 +17,7 @@ const web3 = new Web3(
 async function farm(toAddress, abi) {
 	const nonce = await web3.eth.getTransactionCount(ownerAddress);
 	const gasPriceWei = await web3.eth.getGasPrice();
-	const strategy = new web3.eth.Contract(STRATEGY_ABI, toAddress);
+	const strategy = new web3.eth.Contract(abi, toAddress);
 	const data = strategy.methods.harvest(toAddress).encodeABI()
 
 	console.log("farm staking")
