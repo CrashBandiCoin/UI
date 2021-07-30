f5ae560d330a948f9ece0894eeb0da82ab72c97f
Developer <85023510+carlatic-dev@users.noreply.github.com>
2021-07-30 14:31:30 -0700

    Update vaults.ts

--- src/config/constants/vaults.ts
+++ src/config/constants/vaults.ts
@@ -4,7 +4,7 @@ import { FarmConfig, QuoteToken, ContractType } from './types'
 const farms: FarmConfig[] = [
 	{
 	    id: 1,
-	    pid: 3, // TODO fixe the correct APR
+	    pid: 2, // TODO fixe the correct APR
 	    type: ContractType.Sugar,
 	    risk: 5,
 	    isTokenOnly: true,
@@ -27,7 +27,7 @@ const farms: FarmConfig[] = [
 	},
 	{
 	    id: 3,
-	    pid: 2,
+	    pid: 1,
 	    type: ContractType.Sugar,
 	    risk: 5,
 	    isTokenOnly: true,
