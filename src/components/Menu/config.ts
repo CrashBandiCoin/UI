4e84538607b0ca9e6fef6a37714d452e9174c703
CrashBandiCoin <77974644+CrashBandiCoin@users.noreply.github.com>
2021-08-16 19:00:44 +0200

    Update config.ts
    update menu

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -8,31 +8,8 @@ const config: MenuEntry[] = [
 
     },
     {
-        label: 'MINT',
-        icon: 'MintIcon',
-        items: [
-            {
-                label: 'Pools',
-                href: '/pools/mint',
-                status: {
-                    text: "INACTIVE",
-                    color: "failure",
-                }
-            },
-            {
-                label: 'Exchange',
-                href: 'https://pancakeswap.finance/swap?outputCurrency=0x2Deb28ec61E7B6B4Bba5f8398398330227Cd293f',
-            },
-            {
-                label: 'Chart',
-                href: 'https://dex.guru/token/0x2Deb28ec61E7B6B4Bba5f8398398330227Cd293f-bsc',
-            },
-
-        ],
-    },
-    {
-        label: 'SUGAR',
-        icon: 'SugarIcon',
+        label: 'Yield Farming',
+        icon: 'FarmIcon',
         items: [
             {
                 label: 'Farms',
@@ -43,63 +20,33 @@ const config: MenuEntry[] = [
                 href: '/pools/sugar',
             },
             {
-                label: 'Exchange',
+                label: 'Vaults',
+                href: '/vaults',
+            },
+            
+        ],
+    },
+
+    {
+        label: 'BUY/SELL',
+        icon: 'TradeIcon',
+        items: [
+            {
+                label: 'MINT',
+                href: 'https://pancakeswap.finance/swap?outputCurrency=0x2deb28ec61e7b6b4bba5f8398398330227cd293f',
+            },
+            {
+                label: 'SUGAR',
                 href: 'https://pancakeswap.finance/swap?outputCurrency=0x41AA9F842AF935cC71252C0dE4BFF13F821546b8',
             },
             {
-                label: 'Chart',
-                href: 'https://dex.guru/token/0x41AA9F842AF935cC71252C0dE4BFF13F821546b8-bsc',
-            },
-        ],
-    },
-    {
-        label: 'TeaSport',
-        icon: 'TeaSportV1Icon',
-        items: [
-            {
-                label: 'OLYMPIC GAMES',
-                href: 'https://teaswap.gitbook.io/teaswap/features/sportparties/olympics-games',
-                status: {
-                    text: "LIVE",
-                    color: "success",
-                },
-            },
-            {
-                label: 'Farms',
-                href: '/farms/teasport',
-                status: {
-                    text: "INACTIVE",
-                    color: "failure",
-                }
-            },
-            {
-                label: 'Pools',
-                href: '/pools/teasport',
-                status: {
-                    text: "INACTIVE",
-                    color: "failure",
-                }
-            },
-            {
-                label: 'Exchange',
+                label: 'TEASPORT',
                 href: 'https://pancakeswap.finance/swap?outputCurrency=0xFc5e7Bed6abA672c6d435715bA931fB75EebFd2A',
             },
-            {
-                label: 'Chart',
-                href: 'https://dex.guru/token/0xFc5e7Bed6abA672c6d435715bA931fB75EebFd2A-bsc',
-            },
         ],
     },
-    {
-        label: 'Vaults',
-        icon: 'FarmIcon',
-        href: '/vaults',
-    },
-    {
-        label: 'Zap',
-        icon: 'TradeIcon',
-        href: '/zap',
-    },
+    
+    
     {
         label: 'Vote',
         icon: 'ReferralIcon',
@@ -119,28 +66,6 @@ const config: MenuEntry[] = [
                 href: '/raffleTeasport',
             },
         ]
-    },
-    {
-        label: 'Listing',
-        icon: 'ListingsIcon',
-        items: [
-            {
-                label: 'DappRadar',
-                href: 'https://dappradar.com/binance-smart-chain/defi/teaswap',
-            },
-            {
-                label: 'IcoHolder',
-                href: 'https://icoholder.com/fr/teaswap-1001257',
-            },
-            {
-                label: 'Coinhut',
-                href: 'https://coinhunt.cc/coin/2006554088',
-            },
-            {
-                label: 'Coinsniper',
-                href: 'https://coinsniper.net/coin/1473',
-            },
-        ],
     },
     {
         label: 'More',
