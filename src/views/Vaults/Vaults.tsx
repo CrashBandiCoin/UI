e13f397f54d50fb3f26f24ceca1b140a7cdb545f
Developer <85023510+carlatic-dev@users.noreply.github.com>
2021-08-16 05:17:24 -0700

    Update price

--- src/views/Vaults/Vaults.tsx
+++ src/views/Vaults/Vaults.tsx
@@ -171,7 +171,13 @@ const Vaults: React.FC<FarmsProps> = (vaultsProps) => {
   const farmsList = useCallback(
     (farmsToDisplay: Vault[]): FarmWithStakedValue[] => {
       let farmsToDisplayWithAPY: FarmWithStakedValue[] = farmsToDisplay.map((farm) => {
-        const totalLiquidity = new BigNumber(farm.lpTotalInQuoteToken).times(cakePrice)
+        let tokenPrice = cakePrice
+        if (farm.id === 1) {
+          tokenPrice = new BigNumber("22.15")
+        } else if (farm.id === 3) {
+          tokenPrice = new BigNumber("22.13")
+        }
+        const totalLiquidity = new BigNumber(farm.lpTotalInQuoteToken).times(tokenPrice)
         const lpTotlalInQuoteJSON = new BigNumber(farm.lpTotalInQuoteToken).toJSON();
         const dailyApy = new BigNumber(farm.apr).div(new BigNumber(10).pow(18)).div(new BigNumber(365)).plus(new BigNumber(1))
         const aprJSON = new BigNumber(farm.apr).div(new BigNumber(10).pow(18)).div(new BigNumber(365)).toNumber()
