d3d3ca7d64fc2b52e275742504b242f1b7d5f092
CrashBandiCoin <88516022+adamChafqani@users.noreply.github.com>
2021-09-29 02:21:23 +0200

    update matchdays.ts

--- src/views/Roadmap/Roadmap.tsx
+++ src/views/Roadmap/Roadmap.tsx
@@ -14,7 +14,7 @@ const Roadmap: React.FC = () => {
     return (
             <Hero>
                 {/* eslint-disable-next-line jsx-a11y/iframe-has-title,react/style-prop-object */}
-                <iframe width="1900" height="1000" src="https://view.monday.com/embed/1213066002-f017b3d5e3fc844c2157ea36781fbc65?r=use1"  />
+                <iframe width="1500" height="1000" src="https://view.monday.com/embed/1213066002-f017b3d5e3fc844c2157ea36781fbc65?r=use1"  />
             </Hero>
     )
 }
