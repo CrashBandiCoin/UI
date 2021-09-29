9cdd363ddcc6a02e841a2ac4c5f42fa4c145d4a9
CrashBandiCoin <77974644+CrashBandiCoin@users.noreply.github.com>
2021-09-29 23:00:42 +0200

    update

--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -1,7 +1,25 @@
 import { Ifo } from './types'
 
 const ifos: Ifo[] = [
-
+  {
+    id: 'jaggery',
+    address: '0x915113813ffBCBEb7f26F881bD8a1c7781C52C0F',
+    isActive: true,
+    name: 'Jaggery',
+    subTitle: 'A blockchain based mutual to share the risk of DeFi on RSK',
+    description:
+      'Earn Jaggery the new token of RSK Blockchain !',
+    launchDate: 'Sept. 30',
+    launchTime: '',
+    saleAmount: '5000',
+    raiseAmount: '0',
+    cakeToBurn: '$0',
+    projectSiteUrl: 'https://tea-swap.finance',
+    currency: 'MINT-SUGAR LP',
+    currencyAddress: '0x45cE45a7d69084c1E3E7Ff29B634Bcb872f62d88',
+    tokenDecimals: 18,
+    releaseBlockNumber: 3179990,
+  },
 
 ]
 
